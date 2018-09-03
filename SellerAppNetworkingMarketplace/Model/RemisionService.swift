ef974f2ff8ad048c4218d2f0a98d503c98f2c0a1
Oscar Morales <oamorales@grupoasesores.com.mx>
2018-09-03 15:07:13 -0500

    Service to create Order
    Service to create Order

---
+++ SellerAppNetworkingMarketplace/Model/RemisionService.swift
@@ -0,0 +1,18 @@
+//
+//  RemisionService.swift
+//  SellerAppNetworkingMarketplace
+//
+//  Created by Oscar Morales on 03/09/18.
+//  Copyright © 2018 Liverpool. All rights reserved.
+//
+
+import Foundation
+
+public typealias errorHandler = (_ errorString:String) -> Void
+
+public class RemisionService
+{
+    class public func createMKPOrder(parameters: [AnyObject], completion:@escaping (_ dataResponse: responseOrder)-> Void, completionError: @escaping errorHandler){
+        
+    }
+}
